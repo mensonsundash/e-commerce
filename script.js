f3a732aa9e2ca475d761c06cb90486247a01b9a9
menso431 <menson.sundash@gmail.com>
2025-06-07 19:42:22 +1000

    Display filtered product as EventListeners

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const products = [
   { id: 4, name: "Jeans", category: "clothing", price: 39.99, inStock: true }
 ];
 
+
+/**
+ * Utility Functions
+ */
+
 //filter bycategory
 function filterByCategory(products, category, partial = false) {
     
@@ -49,3 +54,66 @@ function getAvailableProducts(products) {
     return products.filter(product => product.inStock);
 }
 
+
+/**
+ * Resulting Functions
+ */
+//filtered product combined
+function getFilteredProducts() {
+    let filtered = [...products];
+
+    const search = document.getElementById('searchInput').value;
+    const category = document.getElementById('categoryFilter').value;
+    const minPrice = parseFloat(document.getElementById('minPrice').value) || 0;
+    const maxPrice = parseFloat(document.getElementById('maxPrice').value);
+    const inStockOnly = document.getElementById('inStockOnly').checked;
+
+    if(search) filtered = searchByKeyword(filtered, search);
+    if(category) filtered = filterByCategory(filtered, category);
+    if(minPrice || maxPrice) filtered = filterByPriceRange(filtered, minPrice, maxPrice);
+    if(inStockOnly) filtered = getAvailableProducts(filtered);
+    
+    displayProducts(filtered);
+}
+
+//function to display products UI Rendered
+function displayProducts(productArray) {
+    const container = document.getElementById('productList');
+
+    container.innerHTML = '';
+
+    if(productArray.length === 0) {
+        container.innerHTML = '<p> No Product Found.</p>';
+        return;
+    }
+
+    productArray.forEach(product => {
+        const card = document.createElement('div');
+        card.className = 'product-card';
+        card.innerHTML = `
+            <h3>${product.name}</h3>
+            <p>Category: ${product.category}</p>
+            <p>Price: ${product.price.toFixed(2)}</P>
+            <p>${product.inStock ? "✅ In Stock" : "❌ Out of Stock"}</p>
+            <button class="add-to-cart-btn" data-id="${product.id}">Add to cart</button>
+        `;
+        container.appendChild(card);
+    });
+
+}
+
+
+//Event listeners
+["searchInput", "categoryFilter", "minPrice", "maxPrice", "inStockOnly"].forEach( id => {
+    document.getElementById(id).addEventListener("input", getFilteredProducts);
+    document.getElementById(id).addEventListener("change", getFilteredProducts);
+})
+
+// document.getElementById('searchInput').addEventListener("input", getFilteredProducts);
+// document.getElementById('categoryFilter').addEventListener("change", getFilteredProducts);
+// document.getElementById('minPrice').addEventListener("input", getFilteredProducts);
+// document.getElementById('maxPrice').addEventListener("input", getFilteredProducts);
+// document.getElementById('inStockOnly').addEventListener("input", getFilteredProducts)
+
+// Initial display
+getFilteredProducts();
