4381d16ca5c63e0f28f7ddf7b3eb8606290f89fe
menso431 <menson.sundash@gmail.com>
2025-06-19 13:27:16 +1000

    Creating separate admin page

--- admin-script.js
+++ admin-script.js
@@ -3,12 +3,19 @@ const loggedInUser = JSON.parse(sessionStorage.getItem("loggedInUser"))
 if(!loggedInUser || loggedInUser.role !== "admin"){
     alert('Access denied. Only admins allowed.')
     window.location.href = "index.html";
+    
 } else {
-    document.getElementById("adminWelcome").textContent = `Welcome back, Admin ${loggedInUser.name}`;
+    document.getElementById("adminWelcome").textContent = `Welcome back, ${loggedInUser.name}`;
 
 }
 
 function logoutAdmin() {
     sessionStorage.removeItem("loggedInUser");
     window.location.href = "index.html"
+}
+
+function toggleProfile() {
+
+    const popup = document.getElementById("profilePopup");
+    popup.style.display = popup.style.display === "block" ? "none": "block";
 }
